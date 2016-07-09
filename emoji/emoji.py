a4c6d8a87fb9917b9edd6950e73a9c8b20e67acb
Steve Clement <steve@localhost.lu>
2016-07-09 18:42:04 +0200

    - Updates, templates, css, setup.cfg etc

--- emoji/emoji.py
+++ emoji/emoji.py
@@ -1,9 +1,113 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 # all the imports
 import os
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
+import urllib.request
 
+import json
 
+from time import time
+
+# create our little application :)
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+# Load default config and override config from an environment variable
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, 'emoji.db'),
+    SECRET_KEY='d3v3l0pm3nt k3y',
+    USERNAME='admin',
+    PASSWORD='a55w0rd',
+		APIURL='https://api.github.com/emojis'
+))
+app.config.from_envvar('EMOJI_SETTINGS', silent=True)
+
+def update_db():
+    response = urllib.request.urlopen(app.config['APIURL'])
+    data = response.read
+    text = data.decode('utf-8')
+    json_acceptable_string = text.replace("'", "\"")
+    emoList = json.loads(json_acceptable_string)
+    for name in dict.keys(emoList):
+        print("<a href={}>{}</a>".format(emoList[name], name))
+
+def connect_db():
+    """Connects to the specific database."""
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+
+def init_db():
+    """Initializes the database."""
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+@app.cli.command('initdb')
+def initdb_command():
+    """Creates the database tables."""
+    init_db()
+    print('Initialized the database.')
+
+def get_db():
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database again at the end of the request."""
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
+@app.route('/')
+def show_entries():
+    db = get_db()
+    cur = db.execute('SELECT name, imgURL FROM entries ORDER BY id DESC')
+    entries = cur.fetchall()
+    return render_template('show_entries.html', entries=entries)
+
+@app.route('/add', methods=['POST'])
+def add_entry():
+    if not session.get('logged_in'):
+        abort(401)
+    db = get_db()
+    db.execute('insert into entries (title, text) values (?, ?)',
+                 [request.form['title'], request.form['text']])
+    db.commit()
+    flash('New entry was successfully posted')
+    return redirect(url_for('show_entries'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('show_entries'))
+
+if __name__ == '__main__':
+    init_db()
+    update_db()
+    app.run(debug=True)
